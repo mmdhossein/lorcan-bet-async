dc676e3d1d55530f34cdf2456441959a4025e43e
Hossein <65893340+mmdhossein@users.noreply.github.com>
2024-09-22 15:17:06 +0330

    Update app.service.ts

--- order/src/app.service.ts
+++ order/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class OrderService {
+  constructor(@Inject('ORDER_SERVICE') private orderBrokerServices: ClientKakfa, @InjectRepository(Order) private orderRepository: Repository<Order>) {}
+  
+  
+  createOrder(newOrderDto:Order) {
+    const log = new OrderLog()
+    try{
+    await this.orderRepository.save(newOrderDto)
+    await this.orderBrokerServices.emit('inventory_new_order', order)
+    log.status = LogStatus.SUCCESS
+    }catch(e){
+    log.status = LogStatus.FAILED
+    log.errorMessage = e.message  
+    }
+    finally{
+          await this.orderBrokerServices.emit('log_order', log)
+    }
+
+
   }
 }
