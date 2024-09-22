f80322dbcb696849ad5e0703ab430a3acd6e6ca0
Hossein <65893340+mmdhossein@users.noreply.github.com>
2024-09-22 14:31:01 +0330

    Update order.entities.ts

--- order/src/order.entities.ts
+++ order/src/order.entities.ts
@@ -1,1 +1,14 @@
+import {Entity, Column, PrimaryGeneratedColumn, OneToMany} from 'typeorm';
 
+@Entity()
+export class Order {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    @ManyToMany(()=>Product)
+    @JoinColumn({ name: 'productId', referencedColumnName: 'id' }})
+    product: Product;//todo check this is id if relations:{product:false}
+    @Column()
+    quantity: number;
+
+}
