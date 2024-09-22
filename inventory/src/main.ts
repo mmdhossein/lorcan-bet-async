273534c8ed1c0e2f3f0c4514dbdda35aebd3be1a
Hossein <65893340+mmdhossein@users.noreply.github.com>
2024-09-22 11:55:35 +0330

    Update main.ts

--- inventory/src/main.ts
+++ inventory/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {KafkaOptions, Transport} from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [process.env.KAFKA_HOST],
+      },
+      consumer: {
+        groupId: 'inventory-group',
+      },
+    },
+  }as KafkaOptions);
+  await app.listen();
 }
 bootstrap();
