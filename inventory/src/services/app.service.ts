f5cb5a8e31db4865d07687416e64c1d490baed28
Hossein <65893340+mmdhossein@users.noreply.github.com>
2024-09-22 12:32:37 +0330

    Update app.service.ts

--- inventory/src/services/app.service.ts
+++ inventory/src/services/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+export class InventoryService {
+    constructor(@InjectRepository(Inventory) private inventoryRepository: Repository<Inventory>) {
+
+    }
+  
+  //this should be transactional
+     async reserveInventory(productId:number, quantity:number) {
+        try {
+          //check inventory if we have enough quantity and deduce it, can you do it as a procedure or transactional
+          //in condition of not enough send error message
+          await this.inventoryRepository.manager.transaction( async (tranasctionalEntityManager:EntityManager)=>{
+            const inventory = await tranasctionalEntityManager.createQueryBuilder(Inventory, 'inventory').setLocak('pessimistic_write').where('inventory.productId = :productId', {productId}).getOne()
+            if(!inventory){
+              return {error:`inventory for product id: ${productId} not found`}
+            }
+            if(inventory.quantity >= quantity){
+              inventory.quantity -= quantity
+              await tranasctionalEntityManager.save(inventory)
+            }else{
+              return {error:`inventory quantity is not sufficient, quantity available: ${inventory.quantity}`}
+            }
+          })
+        
+            console.log('reserveInventory')
+            return result
+        } catch (e) {
+            return {error: e.message}
+        }
+
+    }
 }
