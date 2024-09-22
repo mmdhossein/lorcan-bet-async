031a5160bd216c310a21fc2fe5d6db9ebd9854bb
Hossein <65893340+mmdhossein@users.noreply.github.com>
2024-09-22 13:06:07 +0330

    Update app.controller.ts

--- inventory/src/app.controller.ts
+++ inventory/src/app.controller.ts
@@ -4,9 +4,9 @@ import { AppService } from './services/app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+//this service needs to be on eventPattern and also it should have a retry mechanism, once the inventory reserved it should emit an event to the order service to continue the payment
+  @EventPattern('inventory_new_order')
+  reserveInventory(req) {
+    return this.appService.reserveInventory();
   }
 }
